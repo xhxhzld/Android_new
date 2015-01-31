f582852d0ec5824c9ef956ed42561902ac242aba
SangwooChoi2 <chltkddn0102@snu.ac.kr>
2015-02-01 03:25:14 +0900

    graduate 만들기 2

---
+++ app/src/main/java/com/example/sangwoochoi/ie/GraduateActivity2.java
@@ -0,0 +1,23 @@
+package com.example.sangwoochoi.ie;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by SangwooChoi on 2015-02-01.
+ */
+public class GraduateActivity2 extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_graduate2);  // 레이아웃과 java파일을 연결하는 부분
+
+
+
+    }
+}
